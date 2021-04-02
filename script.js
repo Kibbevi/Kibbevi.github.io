6a13d5de695b3328b23b740c91fbc105cb303e80
Kibbevi <64769962+Kibbevi@users.noreply.github.com>
2021-04-02 13:01:24 +0300

    updates

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-// linkitysjuttuja
-const list = document.getElementById("toDoList");
-const inputtext = document.getElementById("inputfield");
-//const form = document.querySelector(".toDoForm");
-const addBtn = document.getElementById("addButton");
-const delBtn = document.getElementById("delete");
+callEvents();
 
-
-
-//lisää tehtävän local storageen ja tarkistaa etttei kenttä ole tyhjä ->ok
-function validate(){
-    //var textfield = document.getElementById("inputfield").value;
-    //var tasks = toDo.elements.todo.value;
-    //var listaus = document.getElementById("toDoList");
-    if(inputtext.value ==""){
-        inputtext.style.border = "solid";
-        inputtext.style.borderColor = "red";
-        alert("Älä jätä kenttää tyhjäksi.");
-        return false;
-        
+function callEvents(){
+  document.querySelector('form').addEventListener('submit',validate);
+  document.getElementById('clear').addEventListener('click',clearAllTasks);
+  //document.querySelector('ul').addEventListener('click',deleteorCheck);
+}
+// validate
+function validate(e){
+    e.preventDefault();
+    let input = document.querySelector('input');
+    
+    if(input.value ==""){
+      input.style.border="solid";
+      input.style.borderColor ="red";
+      alert("Älä jätä kenttää tyhjäksi");
+      return false;
     }
-    //var li = document.createElement("li");
-    //li.innerText = inputtext.value;
-    //li.classList.add("agenda");
-    //list.appendChild(li);
-        
-    inputtext.style.border = "none";
+    input.style.border ="none";
+    
+
+    addTask(input.value);
+    input.value = '';
+}
+
+// add tasks
+function addTask(task){
+    //let ul = document.querySelector('ul');
+    //let li = document.createElement('li');
+    //li.innerHTML = `<input type="checkbox"><label>${task}</label><span class="delete">\u00D7</span>`;
+    //ul.appendChild(li);
+    
     let getLclStrgDta = localStorage.getItem("tekemista");
     if (getLclStrgDta) {
         lista = JSON.parse(getLclStrgDta);
     } else {
         lista = [];
     }
-    lista.push(inputtext.value);
+    lista.push(task);
     localStorage.setItem("tekemista", JSON.stringify(lista));
-    
     taskList();
-    //return true;
-    //taskList();
     
-    //taskList();
-    //var listamus = document.getElementById
     
+  
 }
-//lisää ja näyttää kaikki listalla olevat tehtävät -> ok
-function taskList() {
+function taskList(){
+    var list = document.getElementById("toDoList");
     let getLclStrgDta = localStorage.getItem("tekemista");
     if (getLclStrgDta) {
         lista = JSON.parse(getLclStrgDta);
@@ -52,15 +53,28 @@ function taskList() {
     }
     let li = "";
     lista.forEach((element, id) => {
-        li += `<li class ="liTag"><span class="check" onclick="markAsDone(${id})"><i class="fas fa-check"></i></span>${element}<span class="del" onclick="deleteTask(${id})"><i class="fas fa-trash"></i></span></li>`;
+        li += `<li class ="liTag"><input type="checkbox"onclick="check(${id})"><i class="fas fa-check"></i></span>${element}<span class="delete" onclick="deleteTask(${id})">\u00D7</span></li>`;
     });
-    list.innerHTML = li; 
-    inputtext.value = "";
-    
-
+    list.innerHTML = li;
 }
-//poista tehtävä ->ok
-function deleteTask(id) {
+
+function clearAllTasks(e){
+    let ul = document.querySelector('ul').innerHTML = '';
+    localStorage.clear();
+}
+//function deleteorCheck(e){
+    //if(e.target.className == 'delete')
+   //   deleteTask(e);
+    //else {
+    //  check(e);
+    //}
+//}
+  
+  // delete task
+function deleteTask(id){
+    //let remove = e.target.parentNode;
+    //let parentNode = remove.parentNode;
+    //parentNode.removeChild(remove);
     let getLclStrgDta = localStorage.getItem("tekemista");
     if (getLclStrgDta) {
         lista = JSON.parse(getLclStrgDta);
@@ -70,28 +84,39 @@ function deleteTask(id) {
     lista.splice(id, 1);
     localStorage.setItem("tekemista", JSON.stringify(lista));
     taskList();
+    
+    
+
+    
 }
-// merkitsee tehdyksi
-function markAsDone() {
+
+  
+  // tick a task
+function check(id){
     let getLclStrgDta = localStorage.getItem("tekemista");
     if (getLclStrgDta) {
         lista = JSON.parse(getLclStrgDta);
     } else {
         lista = [];
     }
+    let ul = document.querySelector('ul');
+    let li = document.querySelector('liTag');
+    
+    //lista.splice(id, 0);
+    
+    
+    //id.textDecoration ="line-through";
+    //localStorage.setItem("tekemista", JSON.stringify(lista));
+    //taskList();
+    //const task = e.target.nextSibling;
+    //if(e.target.checked){
+      //task.style.textDecoration = "line-through";
+      //task.style.color ="lightslategrey";
+    //}else {
+     // task.style.textDecoration = "none";
+    //  task.style.color = "#2f4f4f";
     
 }
-
-// poistaa kaikki tehtävät listalta ->ok
-function deleteAllTasks() {
-    list.innerHTML ="";
-    localStorage.clear();
-    
-}
-
-
-
-
        
 // tallenna listan tiedot selaimeen ->ok
 // muista siirtää css ja javascript ulkoiseen tiedostoon
